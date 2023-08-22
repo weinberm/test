25e44d3b2b07ca105f79a26e165eebf1e5405a51
weinberger <smw1297@yahoo.de>
2023-08-22 11:24:01 +0200

    Init BackgroundLocation Service, created timer and callback funtion so coord every 10 sec and callback to mapviewmodel

--- lib/viewmodels/map_viewmodel.dart
+++ lib/viewmodels/map_viewmodel.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:flutter_map/flutter_map.dart' as fm;
 import 'package:latlong2/latlong.dart';
+import '../models/Coordinate.dart';
 
 import 'package:waste_walking_ba/services/backgroundlocation_service.dart';
 
@@ -18,7 +19,7 @@ class MapViewModel extends ChangeNotifier {
   List<fm.Marker> markers = [];
 
   bool trackingActive = false;
-  bool lockPosition = false;
+  bool lockPositionValue = true;
   bool hideOtherWalks = false;
 
   late DateTime startTime; // Setze das Startdatum
@@ -27,35 +28,73 @@ class MapViewModel extends ChangeNotifier {
 
   String formattedDuration = "0:00:00";
 
-  void startTracking() {
+  MapViewModel() {
+    print("Init");
+    backgroundLocationService.initBackgroundLocation();
+    backgroundLocationService.onNewCoordinate =
+        (p0) => proccessNewCoordinate(p0);
+    backgroundLocationService.startLocationTimer();
+  }
+
+  void proccessNewCoordinate(Coordinate newCoordinate) {
+    print("Callback Func");
+    print(newCoordinate);
+  }
+
+  void toggleTracking() {
     if (!trackingActive) {
-      startTime = DateTime.now();
-      const duration = Duration(seconds: 1);
-      _timer = Timer.periodic(duration, (Timer timer) {
-        final currentTime = DateTime.now();
-        difference = currentTime.difference(startTime);
-        formattedDuration =
-            difference.toString().split('.')[0]; // Entfernt die Millisekunden
-
-        print(formattedDuration);
-        notifyListeners();
-      });
+      startTimer();
     } else {
-      _timer.cancel();
-      formattedDuration = "0:00:00";
+      stopTimer();
     }
-
     trackingActive = !trackingActive;
     notifyListeners();
   }
 
-  void toggleLockPosition() {
-    lockPosition = !lockPosition;
+  void startTimer() {
+    const duration = Duration(seconds: 1);
+    _timer = Timer.periodic(duration, _updateTimer);
+    startTime = DateTime.now();
+  }
+
+  void _updateTimer(Timer timer) {
+    final currentTime = DateTime.now();
+    difference = currentTime.difference(startTime);
+    formattedDuration = difference.toString().split('.')[0];
+    print(formattedDuration + "New");
+    notifyListeners();
+  }
+
+  void stopTimer() {
+    _timer.cancel();
+    formattedDuration = "0:00:00";
+    notifyListeners();
+  }
+
+  void lockPosition() {
+    lockPositionValue = true;
+    print(lockPositionValue);
+    notifyListeners();
+  }
+
+  void unlockPosition() {
+    lockPositionValue = false;
     notifyListeners();
   }
 
   void toggleHideOtherWalks() {
     hideOtherWalks = !hideOtherWalks;
     notifyListeners();
+  }
+
+  void onPositionChanged(MapPosition position, bool hasGesture) {
+    if (lockPositionValue) {
+      unlockPosition();
+    }
+    print("Center: ${position.center!.latitude} ${position.center!.longitude}");
+    print("Zoom: ${position.zoom}");
+    print(
+        "Bounds: N${position.bounds!.north} E${position.bounds!.east} S${position.bounds!.south} W${position.bounds!.west}");
+    print("Has Gesture: $hasGesture");
   }
 }
