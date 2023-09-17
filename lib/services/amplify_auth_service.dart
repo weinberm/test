c3a9068d3877fb0dfca7115d5ef59d65ccf36a29
weinberm <Marc_Leon.Weinberger@Student.Reutlingen-University.DE>
2023-09-17 15:54:47 +0200

    .

---
+++ lib/services/amplify_auth_service.dart
@@ -0,0 +1,95 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+class AmplifyAuthService {
+  Future<bool> isUserSignedIn() async {
+    final result = await Amplify.Auth.fetchAuthSession();
+    return result.isSignedIn;
+  }
+
+  Future<AuthUser> getCurrentUser() async {
+    final user = await Amplify.Auth.getCurrentUser();
+    return user;
+  }
+
+  Future<String> getCurrentUserId() async {
+    final user = await Amplify.Auth.getCurrentUser();
+    return user.userId;
+  }
+
+  Future<void> signInUser(String username, String password) async {
+    try {
+      final result = await Amplify.Auth.signIn(
+        username: username,
+        password: password,
+      );
+      await _handleSignInResult(result);
+    } on AuthException catch (e) {
+      safePrint('Error signing in: ${e.message}');
+    }
+  }
+
+  Future<void> signOutCurrentUser() async {
+    final result = await Amplify.Auth.signOut();
+    if (result is CognitoCompleteSignOut) {
+      safePrint('Sign out completed successfully');
+    } else if (result is CognitoFailedSignOut) {
+      safePrint('Error signing user out: ${result.exception.message}');
+    }
+  }
+
+  Future<void> confirmNewPassword() async {
+    try {
+      final result = await Amplify.Auth.confirmSignIn(
+        confirmationValue: "123456789MaLe.",
+      );
+      return _handleSignInResult(result);
+    } on AuthException catch (e) {
+      safePrint('Error confirming new password: ${e.message}');
+    }
+  }
+
+  Future<void> _handleSignInResult(SignInResult result) async {
+    switch (result.nextStep.signInStep) {
+      case AuthSignInStep.confirmSignInWithSmsMfaCode:
+        final codeDeliveryDetails = result.nextStep.codeDeliveryDetails!;
+        _handleCodeDelivery(codeDeliveryDetails);
+        break;
+      case AuthSignInStep.confirmSignInWithNewPassword:
+        safePrint('Enter a new password to continue signing in');
+
+        //confirmNewPasswordFunktion
+        break;
+      case AuthSignInStep.confirmSignInWithCustomChallenge:
+        final parameters = result.nextStep.additionalInfo;
+        final prompt = parameters['prompt']!;
+        safePrint(prompt);
+        break;
+      case AuthSignInStep.resetPassword:
+        // final resetResult = await Amplify.Auth.resetPassword(
+        //   username: username,
+        // );
+        // await _handleResetPasswordResult(resetResult);
+        break;
+      case AuthSignInStep.confirmSignUp:
+        // Resend the sign up code to the registered device.
+        // final resendResult = await Amplify.Auth.resendSignUpCode(
+        //   username: username,
+        // );
+        // _handleCodeDelivery(resendResult.codeDeliveryDetails);
+        break;
+      case AuthSignInStep.done:
+        safePrint('Sign in is complete');
+        break;
+      default:
+        print("");
+    }
+  }
+
+  void _handleCodeDelivery(AuthCodeDeliveryDetails codeDeliveryDetails) {
+    safePrint(
+      'A confirmation code has been sent to ${codeDeliveryDetails.destination}. '
+      'Please check your ${codeDeliveryDetails.deliveryMedium.name} for the code.',
+    );
+  }
+}
